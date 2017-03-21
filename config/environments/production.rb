fdb1f55bbe3c41900daf4351f9877b47b350f4de
leyyaps <andrew.smith@ipsofactouk.com>
2017-03-21 15:40:59 +0000

    changes to production and gitignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,9 +57,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ipsofacto_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  Rails.application.routes.default_url_options[:host] = 'crisisnottingham.com'
 
-  config.action_mailer.default_url_options = { :host => 'crisisnottingham.com' }
+  config.action_mailer.default_url_options = { :host => 'ipsofacto-ldn.herokuapp.com' }}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -67,14 +66,13 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
-  }
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'heroku.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
 
 
